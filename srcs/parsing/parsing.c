01abae27bf3eb8ff5d497ffe4882eb8cd7f1bffc
Reallynotanalien <kafortin@student.42quebec.com>
2024-01-13 16:07:47 -0500

    parsing norminette

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -3,47 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edufour <edufour@student.42quebec.com>     +#+  +:+       +#+        */
+/*   By: kafortin <kafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 19:33:52 by kafortin          #+#    #+#             */
-/*   Updated: 2024/01/10 14:21:44 by edufour          ###   ########.fr       */
+/*   Updated: 2024/01/13 16:07:22 by kafortin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-//returns an allocated copy of the environment
-// char	**copy_env(char **env)
-// {
-// 	char	**new_env;
-// 	int		nb_lines;
-// 	int		i;
-
-// 	nb_lines = 0;
-// 	while (env[nb_lines])
-// 		nb_lines++;
-// 	new_env = ft_calloc(nb_lines + 1, sizeof(char *));
-// 	i = -1;
-// 	while (env[++i])
-// 		new_env[i] = ft_strdup(env[i]);
-// 	return (new_env);
-// }
-
-/*Checks if the minishell args are valid and makes a copy
-of the env variable to be used later.*/
-// int	main_parsing(int argc, char **argv, char **env)
-// {
-// 	(void)argc;
-// 	(void)argv;
-// 	// if (argc != 1)
-// 	// 	return (print_error(ARGC_ERROR));
-// 	use_data()->new_env = copy_env(env);
-// 	// if (!use_data()->new_env)
-// 		// return (print_error(ENV_ERROR));
-// 	// if (!argv)
-// 	// 	return (print_error(ARGV_ERROR));
-// 	return (0);
-// }
 
 /*First checks if all the quotes are closed, if not, returns ERROR
 and sets the error flag to ERROR.
@@ -76,15 +43,20 @@ char	*remove_nonascii(char *str)
 	return (new_str);
 }
 
+int	ignore_tabs(int i)
+{
+	while (use_data()->line[i]
+		&& (use_data()->line[i] == ' ' || use_data()->line[i] == '\t'))
+		i++;
+	return (i);
+}
+
 void	line_parsing(void)
 {
 	int		i;
 	char	*tmp;
 
-	i = 0;
-	while (use_data()->line[i]
-		&& (use_data()->line[i] == ' ' || use_data()->line[i] == '\t'))
-		i++;
+	i = ignore_tabs(0);
 	tmp = remove_nonascii(use_data()->line);
 	safe_free ((void **)&(use_data()->line));
 	use_data()->line = tmp;
